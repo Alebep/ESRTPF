825884388633d421bf790b7e070444b3d2bef54f
alebep <alebexalebep@mail.ru>
2022-12-06 04:37:22 +0000

    CheckPoint So falta o monitoramento

--- Router.py
+++ Router.py
@@ -210,7 +210,7 @@ class Bootstrap:
     
     def Getneighbors(self):
         # A variavel dst contem o par ip:porta de quem fez o pedido, o que e feito e ir a lista buscar vizinhos
-        self.__neighbors_addr = Test[self.dst[0]]
+        self.__neighbors_addr = nodes_neighbors[self.dst[0]]#Test[self.dst[0]]
     
     def main(self):
         while True:
@@ -357,7 +357,8 @@ def main():
             tcpBootSocket = {}
             print('bootstrap ativado')
             #ThisNodeAddr = 
-            neighbors = Test[sys.argv[2]]
+            #neighbors = Test[sys.argv[2]] #teste
+            neighbors = nodes_neighbors[sys.argv[2]] #Final
             #print(f"{Port_Boot}")
             tcpBootSocket['tcpSocket'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
             tcpBootSocket['tcpSocket'].bind((sys.argv[2], Port_Boot))
